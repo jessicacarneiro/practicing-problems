fafea6f8f6bb16603a4ab11fcd1c5b66921d1970
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2016-07-06 09:34:18 -0300

    Solved 30 days of code days 0 to 4.

---
+++ hacker_rank/30_days/day1.py
@@ -0,0 +1,19 @@
+# Declare second integer, double, and String variables.
+j = 0
+f = 0.0
+m = ""
+
+# Read and save an integer, double, and String to your variables.
+j = input()
+f = input()
+m = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i + j
+
+# Print the sum of the double variables on a new line.
+print d + f
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + m
