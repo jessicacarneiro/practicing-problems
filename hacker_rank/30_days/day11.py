99b644e1b9614ece3916a3ef3c0f0b55776f0ab3
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2016-07-13 19:03:46 -0300

    Day 11 of 30 days of code solved.

---
+++ hacker_rank/30_days/day11.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys
+
+
+arr = []
+for arr_i in xrange(6):
+    arr_temp = map(int,raw_input().strip().split(' '))
+    arr.append(arr_temp)
+
+max = -1000000000
+for i in xrange(4):
+    for j in xrange(4):
+        s = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+        if s > max:
+            max = s
+        
+print max
